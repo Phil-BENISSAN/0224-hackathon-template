aafc3d52de67984940e9cb5cfb5f1aa3387d8c3d
Phil-BENISSAN <philippe.benissan@wildcodeschool.com>
2024-07-03 17:19:27 +0200

    fastapi refresh

--- data-fastapi/app/main.py
+++ data-fastapi/app/main.py
@@ -14,5 +14,6 @@ def fn_fast_api():
 # ---------------- FIN DE TON CODE ----------------
 #__________________________________________________
 
-if __name__ == '__main__':
-    uvicorn.run(app, port=8000, host='0.0.0.0')
+# PAS NECESSAIRE DANS L'IMAGE DOCKER
+# if __name__ == '__main__':
+#     uvicorn.run(app, port=8000, host='0.0.0.0')
